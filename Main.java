9f92d9922e4e7a3c2c2be9aaa61d1e0b0ff7b4a5
Nick <nicolaspinheirogomes3@gmail.com>
2024-12-01 22:07:54 -0300

    Task3 Crie uma listagem de produtos utlizando variáveis primitivas

--- Main.java
+++ Main.java
@@ -1,14 +1,14 @@
 public class Main{
     public static void main(String[] args){
-        int data1 = 1980;
-        int data2 = 2000;
-        System.out.println(">>> Listagem dos Clientes");
+        double preco1 = 49.95;
+        double preco2 = 1470.76;
+        System.out.println(">>> Listagem dos Produtos");
         System.out.println("------------------------------");
-        System.out.println("Nome: Fulano");
-        System.out.println("Data de Nascimento: " + data1);
+        System.out.println("Nome: Livro");
+        System.out.println("Preço: " + preco1);
         System.out.println("------------------------------");
-        System.out.println("Nome: Sicrano");
-        System.out.println("Data de Nascimento: " + data2);
+        System.out.println("Nome: Celular");
+        System.out.println("Preço: " + preco2);
         System.out.println("------------------------------");
     }
 }
