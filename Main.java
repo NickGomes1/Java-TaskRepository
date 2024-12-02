0c7b153cf166b8e3aa328d939b0b136b04307e4e
Nick <nicolaspinheirogomes3@gmail.com>
2024-12-01 22:13:54 -0300

    Task4 Alterar a listagem dos clientes para utilizar o nome dos clientes em variáveis do tipo String

--- Main.java
+++ Main.java
@@ -1,14 +1,16 @@
 public class Main{
     public static void main(String[] args){
-        double preco1 = 49.95;
-        double preco2 = 1470.76;
-        System.out.println(">>> Listagem dos Produtos");
+        int data1 = 1980;
+        int data2 = 2000;
+        String c1 = "Fulano";
+        String c2 = "Sicrano";
+        System.out.println(">>> Listagem dos Clientes");
         System.out.println("------------------------------");
-        System.out.println("Nome: Livro");
-        System.out.println("Preço: " + preco1);
+        System.out.println("Nome: " + c1);
+        System.out.println("Data de Nascimento: " + data1);
         System.out.println("------------------------------");
-        System.out.println("Nome: Celular");
-        System.out.println("Preço: " + preco2);
+        System.out.println("Nome: " + c2);
+        System.out.println("Data de Nascimento: " + data2);
         System.out.println("------------------------------");
     }
 }
