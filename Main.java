ff62172289e6c33e7ab777a7cd616a3ae674d2c6
Nick <nicolaspinheirogomes3@gmail.com>
2024-12-01 22:22:03 -0300

    Task5 Alterar a listagem dos clientes para exibir a característica etária dos clientes

--- Main.java
+++ Main.java
@@ -2,15 +2,35 @@ public class Main{
     public static void main(String[] args){
         int data1 = 1980;
         int data2 = 2000;
+        int anoAtual = 2023;
         String c1 = "Fulano";
-        String c2 = "Sicrano";
+        String c2 = "Beltrano";
         System.out.println(">>> Listagem dos Clientes");
+        System.out.println(">>> Ano atual: " + anoAtual);
         System.out.println("------------------------------");
         System.out.println("Nome: " + c1);
         System.out.println("Data de Nascimento: " + data1);
+        System.out.println("Idade: " + (anoAtual - data1));
+        if(anoAtual - data1 <= 17)
+            System.out.println("Característica Etária: ADOLESCENTE");
+        else if(anoAtual - data1 <= 29)
+            System.out.println("Característica Etária: JOVEM");
+        else if(anoAtual - data1 <= 59)
+            System.out.println("Característica Etária: ADULTO");
+        else
+            System.out.println("Característica Etária: IDOSO");
         System.out.println("------------------------------");
         System.out.println("Nome: " + c2);
         System.out.println("Data de Nascimento: " + data2);
+        System.out.println("Idade: " + (anoAtual - data2));
+        if(anoAtual - data2 <= 17)
+            System.out.println("Característica Etária: ADOLESCENTE");
+        else if(anoAtual - data2 <= 29)
+            System.out.println("Característica Etária: JOVEM");
+        else if(anoAtual - data2 <= 59)
+            System.out.println("Característica Etária: ADULTO");
+        else
+            System.out.println("Característica Etária: IDOSO");
         System.out.println("------------------------------");
     }
 }
