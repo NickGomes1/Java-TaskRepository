a966f15e1d647d38dfdeceba77ffa8a36a2940c0
Nick <nicolaspinheirogomes3@gmail.com>
2024-12-01 22:05:22 -0300

    Task2 Crie uma listagem de clientes utilizando variáveis primitivas

--- Main.java
+++ Main.java
@@ -1,5 +1,14 @@
 public class Main{
     public static void main(String[] args){
-        System.out.println("Seja bem indo ao COMEX!");
+        int data1 = 1980;
+        int data2 = 2000;
+        System.out.println(">>> Listagem dos Clientes");
+        System.out.println("------------------------------");
+        System.out.println("Nome: Fulano");
+        System.out.println("Data de Nascimento: " + data1);
+        System.out.println("------------------------------");
+        System.out.println("Nome: Sicrano");
+        System.out.println("Data de Nascimento: " + data2);
+        System.out.println("------------------------------");
     }
 }
